73550e99799352f0651ae165312ff72fbe4d2fd8
baobab67 <roy.lurie@gmail.com>
2016-03-13 16:25:27 -0400

    suppress -ll output from rtl_tcp

--- start.sh
+++ start.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 echo "Starting rtl_tcp in background"
-rtl_tcp &   # run in background 
+# rtl_tcp > /dev/null &   # run in background and suppress ll
+rtl_tcp 2> >(grep -v 'll' 1>&2) # suppress stderr that starts "ll"
 
-echo "Waiting 15 seconds to start rtlamr"
+echo "Waiting 15 seconds for rtl_tcp to start"
 sleep 15 # wait for rtl_tcp tp start
 
 echo "Starting rtlamr"
