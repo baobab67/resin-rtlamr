f0a7fe15b109b30b48a1a84e0304fc8618463618
baobab67 <roy.lurie@gmail.com>
2016-03-13 13:15:13 -0400

    Wait for 15 seconds for rtl_tcp to start

--- start.sh
+++ start.sh
@@ -3,6 +3,9 @@
 echo "Starting rtl_tcp in background"
 rtl_tcp &   # run in background 
 
+echo "Waiting 15 seconds to start rtlamr"
+sleep 15 # wait for rtl_tcp tp start
+
 echo "Starting rtlamr"
 rtlamr
 
